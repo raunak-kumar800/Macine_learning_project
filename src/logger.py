01edebe5a7aa4372dc30d0118b910d567e78d570
raunak-kumar800 <23f100404@ds.study.iitm.ac.in>
2026-01-15 13:13:27 +0530

    code pushed

---
+++ src/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime
+from logger import *
+
+LOG_File = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(),"logs")
+os.makedirs(log_path,exist_ok=True)
+LOG_File_PATH = os.path.join(log_path,LOG_File)
+
+logging.basicConfig(
+    filename = LOG_File_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+)
+
+if __name__ == "__main__":
+    logging.info("Logging has started")
